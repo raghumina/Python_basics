df69e83a5c06a59277c367c27ed6c63d3b186dcb
raghu <raghumina11@gmail.com>
2020-05-13 22:42:23 -0700

    Comparision Operator in Python

---
+++ ComparisionOperator.py
@@ -0,0 +1,24 @@
+# Comparision operators in python 
+
+x = 10
+y = 12
+
+#print('x.y is',x>y)
+
+# Output: x > y is False
+print('x > y is',x>y)
+
+# Output: x < y is True
+print('x < y is',x<y)
+
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)
