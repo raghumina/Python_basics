0a9e2abb331cec12d26a6c624ffd3668341d6314
raghu <raghumina11@gmail.com>
2020-05-14 06:44:51 -0700

    Indetation in Python

---
+++ Indentetion.py
@@ -0,0 +1,15 @@
+# Indentation 
+# INDENTATION IS MUST IN PYTHON 
+
+colors=('red'
+        'blue'
+        'green')
+print(colors)
+
+a = 1;b =2;c=3
+print(a,b,c)
+
+for i in range(999,1001):
+    print(i)
+    if i== 1:
+        break
---
+++ PythonKeywords.py
@@ -0,0 +1,12 @@
+colors=('red'
+        'blue'
+        'green')
+print(colors)
+
+a = 1;b =2;c=3
+print(a,b,c)
+
+for i in range(999,1001):
+    print(i)
+    if i== 1:
+        break
