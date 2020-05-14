7dcf3e500293559cb2c176aaeecce1338fb60298
raghu <raghumina11@gmail.com>
2020-05-13 22:48:31 -0700

    Logical Operator in Python

---
+++ LogicalOperator.py
@@ -0,0 +1,14 @@
+# Logical Operator
+# and true if both operabds are true 
+# or true is one of operand is true 
+# not true if operand is false 
+
+
+x = True
+y = False
+
+print('x and y is',x and y)
+
+print('x or y is',x or y)
+
+print('not x is',not x)
