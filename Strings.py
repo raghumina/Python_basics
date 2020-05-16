6e1a8ba8d8a6bfb4597ed8eb8387242f8f837713
raghu <raghumina11@gmail.com>
2020-05-16 03:24:00 -0700

    Quotes and Strings ( Multi Line )

---
+++ Strings.py
@@ -0,0 +1,14 @@
+# Quotes
+print("You're doing great!")
+print('You\'re doing great!')
+# example 2
+print("Have you read \"Hamlet\"?")
+print('Have you read "Hamlet"?')
+
+
+# Multi line strings
+print("""This
+is
+a
+multi-line
+string""")
