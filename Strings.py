01b2c8a68955a7d30efb7966d32af21d5d228b2a
raghu <raghumina11@gmail.com>
2020-05-18 22:57:56 -0700

    Largest Number Among Three Number

--- Strings.py
+++ Strings.py
@@ -3,7 +3,7 @@ print("You're doing great!")
 print('You\'re doing great!')
 # example 2
 print("Have you read \"Hamlet\"?")
-print('Have you read "Hamlet"?')
+print('Have you read "Hamlet"?',2)
 
 
 # Multi line strings
@@ -12,5 +12,3 @@ is
 a
 multi-line
 string""")
-
-print("' " ' ")
