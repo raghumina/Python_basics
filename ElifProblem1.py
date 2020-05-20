ac311fdcc228da353c14f8018ebc73544e233329
raghu <raghumina11@gmail.com>
2020-05-20 03:17:54 -0700

    Largest number among three numbers

---
+++ ElifProblem1.py
@@ -0,0 +1,15 @@
+# Python problem using if else 
+# finding the largest number in three numbers 
+print("Please give three numbers")
+a = 1
+b = 5
+c = 3 
+if a>b:
+    print("a is greater than b")
+if a>c:
+    print("a is greate than c ")
+    print("a is the largest number ")
+elif b>c:
+    print(" b is the largest number ")
+else:
+    print("c largest number ")            
