bfda943ae3840592036187781c507cc4aba5a3cf
raghu <raghumina11@gmail.com>
2020-05-19 19:33:49 -0700

    If condition in Python

---
+++ If.py
@@ -0,0 +1,38 @@
+# # If in Python 
+# # a basic examples
+
+# # example 1 
+# a = 23
+# if a == 23:
+#    print("great")
+
+# # Example 2 
+# print("PLEASE PUT YOUR ANSER AS YES OR NO")  # python is a case sensative language so follow the rules 
+# answer = input()
+# if answer == "YES":  # User input sholuld be in Upper cases 
+#     print("You completed a second example in python")  # it will only check if condition if it is in indentations of if 
+# print("Thats all for now ")  # Indentations are very important in Python 
+
+# # One more example ( example 3 )
+# # favourite football or soccer teams 
+# # for now only taking input of two teams 
+# print ("Please enter the name of your favourite team")
+# team = input()
+# if team == "Barcelona":
+#     print("You support the beast team in the world")
+# if team == "Real":
+#     print("hahahaha")
+# print("thats for now")        
+
+
+# example 4 
+# we will use upper case () function this time 
+# upper case will change the user input in the upper cases 
+print("Please enter your country")
+country = input()
+if country.upper() == "INDIA":  # the upper() function will convert the user input in upper cases 
+    print("congrats you can avail this offer ")
+    print("please proceed with other formalities")
+print("its ohk if you are from another conutry ")
+
+
