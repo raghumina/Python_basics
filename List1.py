a5cbb00d39950ad79c91f019bb96aa74e3cd4e96
raghu <raghumina11@gmail.com>
2020-05-21 05:04:55 -0700

    List Revision1

---
+++ List1.py
@@ -0,0 +1,18 @@
+# Revision for lists 
+# creating list 
+# the first value will be on 0
+guest = ['Abel','Ronaldo','Messsi','Neymar']
+print(guest[0])
+print(guest[3])
+number = [1,2,3,4,5,6,7]
+print(number[-1])
+print(guest[1],number[1])  # Print two list together 
+
+
+# will try how to enter string or number into a list through input 
+print("please enter your name ")
+name = input()
+guestName = [name]
+print(guestName[2])
+
+
