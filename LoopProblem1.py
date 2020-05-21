adb13c741f0c42fa805bdc525fac3d0d8821d1b0
raghu <raghumina11@gmail.com>
2020-05-20 23:13:36 -0700

    User input in loops

---
+++ LoopProblem1.py
@@ -0,0 +1,10 @@
+# problem 1
+# ask user for input in the loops 
+
+import turtle
+print("please enter the values that you want to enter ")
+steps1 = int(input())
+for steps in range(steps1):
+    turtle.color('yellow')
+    turtle.forward(150)
+    turtle.right(90)
