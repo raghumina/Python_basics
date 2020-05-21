d8697a20983d7428c9c9e616f05f2a095f84c3dd
raghu <raghumina11@gmail.com>
2020-05-21 04:07:37 -0700

    While Loop Problem 1.

---
+++ WhileLoop1.py
@@ -0,0 +1,16 @@
+# A problem 
+# random one while using for and while loops 
+
+
+#Take 10 integers from keyboard using loop and print their average value on the screen.
+
+print(" Please enter your number")
+sum = 0
+
+i = 10
+while i >0:
+      num = input()
+      sum = sum + num
+      i = i-1
+      avg = sum/10
+      print(avg)
