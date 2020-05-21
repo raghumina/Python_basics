805495d9b12dae724e1da019c33a061d3eadfb38
raghu <raghumina11@gmail.com>
2020-05-20 21:30:02 -0700

    Turtle in Python

---
+++ Turtle.py
@@ -0,0 +1,29 @@
+# here we use the turtle
+# turtle is a package in python
+# it perform various tasks related to symbols loops etc lets see 
+
+# import turtle
+# #turtle.color('red')  # Gives the desired color to the line
+# #turtle.forward(100)  # It shows a line moving forward (the given numerical value is the size of pixals)
+# turtle.right(90)  # turns 90 degree right , the values in the left or right bracet are angles 
+# turtle.color('green')
+# turtle.forward(100)   # the values in forward and backward bracket are the size of ppixals 
+# turtle.left(90)
+# turtle.color('blue')
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.backward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+# the output will be a square 
+# another easy way to do it is 
+# here range is a keyword
+# steps is the name of the varaible 
+import turtle
+for steps in range(4): # it will decide how many time you want to repeat this function 
+    turtle.color('red')
+    turtle.forward(100)  # It shows a line moving forward (the given numerical value is the size of pixals)
+    turtle.left(90)
+    
+
