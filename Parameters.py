8cddf14555da09ce549d7ae62876b746a7963b5e
raghu <raghumina11@gmail.com>
2020-05-22 04:26:27 -0700

    function parameters

---
+++ Parameters.py
@@ -0,0 +1,16 @@
+# Parameters in function 
+# the introdunction of parameters in function 
+
+# lets create a new function 
+
+def main():
+    palyerNames()
+    return
+
+def palyerNames():
+    playerNames = ['Messi','Neymar','Suarez','Xavi']
+    for playerNames in playerNames:
+        print(playerNames)
+    return 
+
+main()
