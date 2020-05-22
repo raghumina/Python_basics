6d32f542fdeca4e4e6c215a1feef16f899194030
raghu <raghumina11@gmail.com>
2020-05-22 10:38:47 -0700

    Try and Except

---
+++ TryExcept.py
@@ -0,0 +1,17 @@
+# TRY AND EXCEPT 
+# we can use them in passswords 
+try:
+    rates = int(input("Plesase enter the rates"))
+except:
+    print("Please enter a valid number ")
+    rates = int(input("Plesase enter the rates"))
+
+
+# another exampel 
+# for example a password 
+
+try:
+    password = int(input("Please enter valid passcode\n"))
+except:
+    print("Ones last chance ")
+    password = int(input("Please enter valid passcode\n"))      
