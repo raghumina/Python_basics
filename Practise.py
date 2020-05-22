5b8e22a84d57b41e83960112961bffaf14876046
raghu <raghumina11@gmail.com>
2020-05-22 09:38:17 -0700

    Revision 1 Turtle,Basic Operations

---
+++ Practise.py
@@ -0,0 +1,37 @@
+# import turtle
+
+# Counter = 0
+# while Counter < 4:
+#  	turtle.forward(100)
+#  	turtle.right(90)
+
+
+# sal = input("write down your salary ")
+
+# # sal_f = float(sal)  # float will convert the variable into the float 
+# # print(sal)
+
+
+# # concatination of the strings 
+# sal = '1321 dollar '
+# question = "how much you earn "
+# print(question + sal)
+
+# # or another way is the 
+# print("hello "+"tom "+"how are you ")
+
+
+# # Print multiple time 
+# print("i will not make syntax error again"* 10)
+# # and to write in next line add \n
+# print("i will not make syntax error again\n"*10)
+
+# # and to add tab or space
+# print("hello\t"*5)
+
+# # Write a program to prompt the user for hours and rate per hour
+# #  to compute gross pay
+hours = int(input("Enter hours\n"))
+rate = int(input("Enter the rate\n"))
+gross_Rate = hours * rate 
+print("So the rate per hour is ",gross_Rate)
