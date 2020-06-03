d47248f0002845a03c932793c13dda5e5a1f3e5a
raghu <raghumina11@gmail.com>
2020-06-03 22:44:28 +0530

    Update WhileLoop1.py

--- WhileLoop1.py
+++ WhileLoop1.py
@@ -4,13 +4,15 @@
 
 #Take 10 integers from keyboard using loop and print their average value on the screen.
 
-print(" Please enter your number")
-sum = 0
+# print(" Please enter your number")
+# sum = 0
 
-i = 10
-while i >0:
-      num = input()
-      sum = sum + num
-      i = i-1
-      avg = sum/10
-      print(avg)
+# i = 10
+# while i >0:
+#       num = input()
+#       sum = sum + num
+#       i = i-1
+#       avg = sum/10
+#       print(avg)
+
+
