90adb4adda5b81bc68333973b6db94ccee400e6f
raghu <raghumina11@gmail.com>
2020-09-03 22:34:59 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,14 +1,30 @@
 # the puyhon data types 
 # the int data type 
 # int is generally used for the numbers 
-int 0
+##int 0
 # string  data type 
 # this data type is used for the characters in the programm 
-str "hello","byyy"
+##str "hello","byyy"
 # boolean data types they are some sort of conditional data types 
 # like yes or no 
 # for example 
-bool True , False 
+##bool True , False 
 # float data type 
-float 0.111, 21.2323
+##float 0.111, 21.2323
 
+
+# creating vairables 
+# ofcourse with data types :) :) 
+# so varaibles with different different data types 
+# 1. 
+x = 1   # int data type 
+y = 2   # int data type 
+z = x + y 
+print(z)   # well the output will be x + y which is 3 
+
+# other varaible with other data type 
+# string 
+
+name  = "Tim"
+city = "Texas"
+print(name,"lives in",city)  # so the output will be "Tim lives in Texas"
