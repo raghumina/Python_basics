f8d984694a19c774de79d25ff09f0d8928df6dab
raghu <raghumina11@gmail.com>
2020-09-03 22:26:30 +0530

    Create Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,14 @@
+# the puyhon data types 
+# the int data type 
+# int is generally used for the numbers 
+int 0
+# string  data type 
+# this data type is used for the characters in the programm 
+str "hello","byyy"
+# boolean data types they are some sort of conditional data types 
+# like yes or no 
+# for example 
+bool True , False 
+# float data type 
+float 0.111, 21.2323
+
