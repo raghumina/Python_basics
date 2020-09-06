74b97cb12a43aa3fe0c9a68b447962fbe4e79ebe
raghu <raghumina11@gmail.com>
2020-09-06 21:35:51 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+# type casting with different different variables 
+##a = "Tom "
+##b = 22 
+##print(a + b)
+
+# now i will use the type casting 
+# type casting is changing one data type to another 
+
+a = "tom"
+b = str(22)
+print(a + b) 
