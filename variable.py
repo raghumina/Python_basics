9838b1c52906fb4d2a306ddd1732181e29593da6
raghu <raghumina11@gmail.com>
2020-09-06 21:50:17 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -2,10 +2,20 @@
 ##a = "Tom "
 ##b = 22 
 ##print(a + b)
-
+# this will show a error in it 
 # now i will use the type casting 
 # type casting is changing one data type to another 
 
-a = "tom"
-b = str(22)
-print(a + b) 
+##a = "tom"
+##b = str(22)
+##print(a + b) 
+
+# now it wil show a output tom22
+
+# lets check another example 
+# lets try to make a simple program  with type casting method 
+
+num1 = input("Please enter a number 1:")
+num2 = input("Please enter number 2:")
+num3 = num1 + num2
+print(num3)
