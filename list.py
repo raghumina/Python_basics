a8062697ea0d866fec48ca6a2004393d24274f14
raghu <raghumina11@gmail.com>
2020-09-07 21:09:36 +0530

    Update list.py

--- list.py
+++ list.py
@@ -2,11 +2,19 @@
 # like other programming languages have array python have list
 # 
 # list are the ordered and mutable collection of the data 
+# lets practice the list 
 
-a = int(input("Please enter the number for your table "))
-b = int(input("Please enter the number for your table "))
-for table in range(1,11):
-    table1 = a * table
-    table2 = b * table
-    print(table1, table2)
-   
+table1 = [1,2,3,45,6,7,8,9]
+table1.append(100)  # to add a value in a list at tje -1 position // we can use .append function for that 
+print(table1)
+
+table2 = ["messi","neymar"]
+# we can also addd values on the specific postion by this method
+table2[1] = "Suarez"
+print(table2)
+
+# lets try another function 
+
+table3 = [11,22,33,55,66,77,88,99]
+table3.insert(3, 44)   # this ,insert function also inserts the values on the defined position 
+print(table3)   
