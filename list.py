f8835f2e61dffb53838a0eb2da77c0ed1f239b00
raghu <raghumina11@gmail.com>
2020-09-07 20:54:31 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# list in python 
+# like other programming languages have array python have list
+# 
+# list are the ordered and mutable collection of the data 
+
+a = int(input("Please enter the number for your table "))
+b = int(input("Please enter the number for your table "))
+for table in range(1,11):
+    table1 = a * table
+    table2 = b * table
+    print(table1, table2)
+   
