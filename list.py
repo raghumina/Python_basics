363ba7dcc3282c6e67fd279bef0ef86005711286
raghu <raghumina11@gmail.com>
2020-09-07 21:20:40 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,18 +3,30 @@
 # 
 # list are the ordered and mutable collection of the data 
 # lets practice the list 
-
+'''
 table1 = [1,2,3,45,6,7,8,9]
 table1.append(100)  # to add a value in a list at tje -1 position // we can use .append function for that 
 print(table1)
+
+# how to add values to the list
 
 table2 = ["messi","neymar"]
 # we can also addd values on the specific postion by this method
 table2[1] = "Suarez"
 print(table2)
-
+'''
 # lets try another function 
 
 table3 = [11,22,33,55,66,77,88,99]
-table3.insert(3, 44)   # this ,insert function also inserts the values on the defined position 
-print(table3)   
+#print(table3)   
+
+# how to delete values from the list
+
+#table3.remove(22)   # .remove function removes the defined values from the list
+#print(table3)
+
+#table3.pop()
+print(table3)    # .pop function automatically removes the last value of the list
+
+del table3[1]
+print(table3)
