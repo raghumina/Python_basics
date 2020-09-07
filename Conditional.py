f946adea7f5e2f23843ae80a5ae7221077b3e6c3
raghu <raghumina11@gmail.com>
2020-09-07 22:38:03 +0530

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,19 @@
+# conditional statements in python 
+# the are just giving more logical power to the program to check and compare and analyse to give the required output 
+
+# 
+# lets write a sample program 
+
+country = str(input("Please enter your country :"))
+age = int(input("Please enter your age :"))
+if country == "INDIA":
+  
+    name = input("Please enter your name :")
+elif country == "USA":
+       
+    name = input("Please enter your name :")
+else:
+    print("Please wait for another list ")       
+
+   
+
