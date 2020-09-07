8092c3f9f2a30bfdd217ef3ccba5d404e824cf3b
raghu <raghumina11@gmail.com>
2020-09-07 22:23:15 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+# Dictionary in python 
+# dictionaries are also collection of data 
+#  it have unordered collection of data 
+
+# lets create a dictionary 
+
+player1 = {
+    "Name" : "Messi",
+    "Season" : 2021,
+    "Goals" : 50,
+    "Assist" : 30
+}
+#print(player1.get("Name"))
+
+#  it ususally uses same functions as list, tuple and set 
