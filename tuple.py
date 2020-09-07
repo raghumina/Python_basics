4735c4dcf001dba028dfa31f096325188f83d820
raghu <raghumina11@gmail.com>
2020-09-07 21:37:00 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,16 @@
+# tuples in python 
+# tuples are also collection of data but immutable we can change the data in it 
+# we use () in tuples 
+
+tuple1 = (1,2,3,4,5)
+print(tuple1)
+
+
+# we can perform typecasting in the tuple 
+# for an example 
+
+var = list(tuple1)
+print(var)  
+
+# now  i can perform the various functions on the tuples 
+# like addition and sunstraction of the values 
