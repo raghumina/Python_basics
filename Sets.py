14f4d6a627cf0cb8171a6f21c43ceaf65a158bea
raghu <raghumina11@gmail.com>
2020-09-07 22:03:03 +0530

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -16,4 +16,23 @@ set1 = {1,2,3,4,4,4,4,4,4,4,4,}
 # and when to add multiple values we will use .update function 
 
 set1.update([22,11,23,3,4,5,6,7,8,9,9.76,5,54,4],["tom"])
+#print(set1)
+
+# to remove 
+# we use .remove function 
+# for example 
+
+#set1.remove(22)
+#print(set1)
+
+# to clear all the set we will use .clear function
+#set1.clear()  # clear will remove all the values from the set 
+#print(set1)
+
+# .discard function this is a linenit function to remove a value it does not show the error if a value is not in a set 
+# for example 
+
+set1.discard("Harrp potter ")
 print(set1)
+
+# we can use .pop, .del, .remove and many other functions which we use on the tuple,  list 
