7295b3550b40790c073506960c76cf4d1c728a8f
raghu <raghumina11@gmail.com>
2020-09-27 22:53:23 +0530

    Update conditional1.py

--- conditional1.py
+++ conditional1.py
@@ -1,4 +1,5 @@
 # 
+'''
 is_hot = True        # only True conditions gets printed 
 cold = False         # false conditions are ignored
 if is_hot:
@@ -9,3 +10,22 @@ elif cold:
 else:
     print("have a good day")
 print("enjoy your day")    
+
+'''
+# lets write weather system 
+# which gives specific messages related to the weather
+import random 
+list = ["Summer","Winter","Monsoon","Spring"]
+mylist = random.choice(list)
+if mylist == "Summer":
+    print("Its a hot day")
+    print("Drink lots of water")
+elif mylist == "Winter":
+    print("Its a cold day")
+    print("Keep yourself warm")
+elif mylist == "Monsoon":
+    print("Its a rainy day")
+    print("Take care")
+else:
+    print("Its a great day")
+    print("enjoy your ")    
