8203ecbaacaa31fa2056f43161806a23da7db954
raghu <raghumina11@gmail.com>
2020-09-27 22:39:18 +0530

    Create conditional1.py

---
+++ conditional1.py
@@ -0,0 +1,11 @@
+# 
+is_hot = True        # only True conditions gets printed 
+cold = False         # false conditions are ignored
+if is_hot:
+    print("Its a hot day")
+    print("drink lots of water")
+elif cold:
+    print("Take care")
+else:
+    print("have a good day")
+print("enjoy your day")    
