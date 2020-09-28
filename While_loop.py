0fec4ec5b29c70b352273cbef46a2a106d059532
raghu <raghumina11@gmail.com>
2020-09-28 19:08:15 +0530

    Create While_loop.py

---
+++ While_loop.py
@@ -0,0 +1,15 @@
+# runs the loop until a condition gets true
+# for example 
+
+a = 10 
+count = 0
+while a < 20:
+    count = count + 1
+    print(count,"hello")
+    a = a + 1
+
+# another example  
+number = 1
+while number <= 5:
+    print("","*" * number)
+    number = number + 1
